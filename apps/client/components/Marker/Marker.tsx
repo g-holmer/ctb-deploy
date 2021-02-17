0deaf3c5c5b2c0e1b4cf722ac5f4be0744740c12
g-holmer <gabbeholmer@hotmail.com>
2021-02-17 11:57:26 +0100

    Add markerCard

--- apps/client/components/Marker/Marker.tsx
+++ apps/client/components/Marker/Marker.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import React, { useState, useRef } from 'react';
-import { Typography, Box } from '@material-ui/core';
+import React, { useState } from 'react';
+
 import styled from 'styled-components';
+import MarkerCard from './MarkerCard';
 
 interface Props {
   companyName: string;
@@ -33,54 +33,22 @@ const Marker = (props: Props) => {
         <InnerMarker />
       </MarkerWrapper>
       {isOpen && (
-        <Card ref={node}>
-          <ImageWrapper>
-            <Image src={props.image} layout="fill" objectfit="contain" />
-          </ImageWrapper>
-          <ListItemDetails>
-            <Typography variant="h6">{props.companyName}</Typography>
-            <Typography>
-              {props.adress.name} {props.adress.postalCode} {props.adress.city}
-            </Typography>
-            <Typography color="secondary">Closed</Typography>
-          </ListItemDetails>
-          {props.distance && <Box>{props.distance} meters away</Box>}
-          <Image
-            src={'/angle-right.svg'}
-            height="20"
-            width="20"
-            objectfit="contain"
-          />
-        </Card>
+        <MarkerCard
+          node={node}
+          key={props.id}
+          companyName={props.companyName}
+          phoneNumber={props.phoneNumber}
+          adress={props.adress}
+          image={props.image}
+          openingHours={props.openingHours}
+          //   distance={navigatorPosition && getDistance(item)} !! DON'T FORGET TO UNCOMMENT THIS LATER!!
+          distance={500} //temporarily
+        />
       )}
     </>
   );
 };
-const ImageWrapper = styled.div`
-  clip-path: circle(50%);
-  position: relative;
-  width: 100px;
-  height: 100px;
-  object-fit: contain;
-`;
-const ListItemDetails = styled(Box)`
-  margin: 20px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
-const Card = styled(Box)`
-  cursor: pointer;
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  background: #ffffff;
-  border-radius: 10px;
-  padding: 20px;
-  margin-bottom: 40px;
-  filter: drop-shadow(0px 6px 10px rgba(0, 0, 0, 0.6));
-  transition: all 0.1s ease-in;
-`;
+
 const MarkerWrapper = styled.div`
   cursor: pointer;
   display: flex;
---
+++ apps/client/components/Marker/MarkerCard.tsx
@@ -0,0 +1,63 @@
+import React, { useRef } from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+interface Props {
+  companyName: string;
+  phoneNumber: string;
+  image: string;
+  openingHours: any;
+  adress: any;
+  distance: number;
+  node: any;
+}
+import { Typography, Box } from '@material-ui/core';
+const MarkerCard = (props: Props) => {
+  return (
+    <Card ref={node}>
+      <ImageWrapper>
+        <Image src={props.image} layout="fill" objectfit="contain" />
+      </ImageWrapper>
+      <ListItemDetails>
+        <Typography variant="h6">{props.companyName}</Typography>
+        <Typography>
+          {props.adress.name} {props.adress.postalCode} {props.adress.city}
+        </Typography>
+        <Typography color="secondary">Closed</Typography>
+      </ListItemDetails>
+      {props.distance && <Box>{props.distance} meters away</Box>}
+      <Image
+        src={'/angle-right.svg'}
+        height="20"
+        width="20"
+        objectfit="contain"
+      />
+    </Card>
+  );
+};
+const ImageWrapper = styled.div`
+  clip-path: circle(50%);
+  position: relative;
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+`;
+const ListItemDetails = styled(Box)`
+  margin: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Card = styled(Box)`
+  cursor: pointer;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 40px;
+  filter: drop-shadow(0px 6px 10px rgba(0, 0, 0, 0.6));
+  transition: all 0.1s ease-in;
+`;
+
+export default MarkerCard;
